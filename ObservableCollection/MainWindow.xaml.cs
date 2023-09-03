cd8a6fb76b14dce6cb3bf2fa4bea1a21ff78e952
Никита Ботов <botov164@gmail.com>
2023-09-03 19:49:27 +0300

    Add project files.

---
+++ ObservableCollection/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Collections;
+
+namespace ListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnClr_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
