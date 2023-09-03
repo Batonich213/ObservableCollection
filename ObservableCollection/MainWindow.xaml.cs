144636873ce7ae836de6bbb1495973180873930b
Никита Ботов <botov164@gmail.com>
2023-09-03 20:32:21 +0300

    YesOrNo Done

--- ObservableCollection/MainWindow.xaml.cs
+++ ObservableCollection/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Collections;
 using System.Collections.ObjectModel;
+using System.Windows.Controls;
 
 namespace ListView
 {
@@ -34,14 +35,21 @@ namespace ListView
 
         private void BtnClr_Click(object sender, RoutedEventArgs e)
         {
-            Entryes.Clear();
+            if (MessageBox.Show("Вы хотите очистить список?", "Выберите", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                Entryes.Clear();
+                
+            }
         }
 
         private void BtnDel_Click(object sender, RoutedEventArgs e)
         {
-
-            Entryes.Remove(lvEntries.SelectedItem.ToString());
-
+            if (MessageBox.Show("Вы хотите удалить?", "Выберите", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                Entryes.Remove(lvEntries.SelectedItem.ToString());
+            }
         }
+
+      
     }
 }
